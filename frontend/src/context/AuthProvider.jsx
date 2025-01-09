86c59a4e0b6e135be176e98324702499769a5ef0
Luis Adrian Ramos <luchoramos042004@gmail.com>
2025-01-09 15:30:24 -0500

    login

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,45 +1,46 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import { createContext } from 'react'
+import axios from "axios"
+import { createContext, useEffect, useState } from "react"
 
 const AuthContext = createContext()
 
+const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState({})
 
-    const perfil = async (token) => {
+    const perfil = async(token) => {
         try {
             const url = `http://localhost:3000/api/perfil`
-            const options = {
-                Headers:{
-                    "Content-Type":"application/json",
-                    Authorization:`Bearer ${token}`
+            const options={
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`
                 }
             }
-            const respuesta = await axios.get(url, options)
+            const respuesta= await axios.get(url,options)
             setAuth(respuesta.data)
-
         } catch (error) {
             console.log(error);
         }
     }
-
     useEffect(() => {
-        const token = localStorage.getItem("token")
-        if (token){
+        const token = localStorage.getItem('token')
+        if(token)
+        {
             perfil(token)
         }
     }, [])
     
-const AuthProvider = ({children}) => {
-    return <AuthContext.Provider value={
-        {
-            auth, setAuth
-        }
-    }/>
+    return (
+        <AuthContext.Provider value={
+            {
+                auth,
+                setAuth              
+            }
+        }>
+            {children}
+        </AuthContext.Provider>
+    )
 }
-
-export{
+export {
     AuthProvider
 }
-
 export default AuthContext
--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -1,8 +1,44 @@
+import axios from 'axios'
+import { useState,useContext } from 'react'
 import {Link} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify';
+import AuthContext from '../context/AuthProvider'
 
 const Login = () => {
+
+    const navigate = useNavigate()
+    const {setAuth} = useContext(AuthContext)
+
+    const [form, setform] = useState({
+        email: "",
+        password: ""
+    })
+
+    const handleChange = (e) => {
+        setform({...form,
+            [e.target.name]:e.target.value
+        })
+    }
+
+	const handleSubmit = async(e) => { 
+	        e.preventDefault()
+	        try {
+	            const url = `http://localhost:3000/api/login`
+	            const respuesta= await axios.post(url,form)
+	            localStorage.setItem('token',respuesta.data.token)
+	            setAuth(respuesta.data)
+	            navigate('/dashboard')
+                toast.success(respuesta.data.msg)
+	        } catch (error) {
+	            toast.error(error.response.data.msg)
+	        }
+	}
+
+
     return (
         <>
+        <ToastContainer />
             <div className="w-1/2 h-screen bg-[url('/public/images/doglogin.jpg')] 
             bg-no-repeat bg-cover bg-center sm:block hidden
             ">
@@ -16,19 +52,25 @@ const Login = () => {
                     <small className="text-gray-400 block my-4 text-sm">Welcome back! Please enter your details</small>
 
 
-                    <form >
+                    <form onSubmit={handleSubmit}>
                         <div className="mb-3">
                             <label className="mb-2 block text-sm font-semibold">Email</label>
-                            <input type="email" placeholder="Enter you email" className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-2 text-gray-500" />
+                            <input type="email" placeholder="Enter you email" 
+                            name='email'
+                            value={form.email || ""} onChange={handleChange}
+                            className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-2 text-gray-500" />
                         </div>
 
                         <div className="mb-3">
                             <label className="mb-2 block text-sm font-semibold">Password</label>
-                            <input type="email" placeholder="********************" className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-2 text-gray-500" />
+                            <input type="password" placeholder="********************" 
+                            name='password'
+                            value={form.password || ""} onChange={handleChange}
+                            className="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-2 text-gray-500" />
                         </div>
 
                         <div className="my-4">
-                            <Link to="/dashboard" className="py-2 w-full block text-center bg-gray-500 text-slate-300 border rounded-xl hover:scale-100 duration-300 hover:bg-gray-900 hover:text-white">Login</Link>
+                            <button className="py-2 w-full block text-center bg-gray-500 text-slate-300 border rounded-xl hover:scale-100 duration-300 hover:bg-gray-900 hover:text-white">Login</button>
                         </div>
 
                     </form>
